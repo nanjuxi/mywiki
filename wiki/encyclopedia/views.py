1bcc350b08384a91f4e65ebc84be53bcbfd43dc3
nanjuxi <1367064846@qq.com>
2021-08-02 15:47:49 +0800

    第一次修改

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
-
 from . import util
+import random
+import markdown2
+from django import forms
+from django.urls import reverse
 
+class EntryForm(forms.Form):
+    title = forms.CharField()
+    content = forms.CharField(widget=forms.Textarea)
 
 def index(request):
+    entries = util.list_entries()
+    rand = random.choice(entries)
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": entries,
+        "random": rand
     })
+
+def entry(request, title):
+    entries = util.list_entries()
+    rand = random.choice(entries)
+    try:
+        content = util.get_entry(title)
+        output = markdown2.markdown(content)
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": output,
+            "random": rand
+        })
+
+    except TypeError:
+        return render(request, "encyclopedia/error.html", {
+            "random": rand
+        })
+
 
