8690eb13845b8a493147dc9f408f7fd5ec766b6c
nanjuxi <1367064846@qq.com>
2021-08-09 16:20:39 +0800

    0809-16:20提交

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
     path("search", views.search, name="search"),
-    path("NewEntry", views.NewEntry, name="NewEntry")
-]
+    path("NewEntry", views.NewEntry, name="NewEntry"),
+    path("NewPage", views.NewPage, name="NewPage")
+    ]
